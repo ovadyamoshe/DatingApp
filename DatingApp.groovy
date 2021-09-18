65680dd5b80d8f848ec6e13f0ef9a4516ecd0d9b
ovadyamoshe <37333126+ovadyamoshe@users.noreply.github.com>
2021-09-18 10:06:17 +0300

    Update DatingApp.groovy

--- DatingApp.groovy
+++ DatingApp.groovy
@@ -1,19 +1,30 @@
 pipeline {
   agent any
+  parameters {
+      choice(name: 'VERSION', choices['1.1.0', '1.2.0', '1.3.0'], description: '')
+      booleanParam(name: 'executeTest', defaulteValue: true, description: '')
+  }
   stages {
-  stage('Stage 1') {
+  stage('builed') {
       steps {
-        script {
-          echo 'Stage 1'
-        }
+        echo "building the application..."
       }
     }
-  stage('Stage 2') {
-      steps {
-        script {
-          echo 'Stage 2'
-        }
+  stage('test') {
+      when {
+          expression {
+              params.executeTest
+          }
       }
+      steps {
+          echo 'testing the applocatin...'
+      }
+    }
+    stage('deploy'){
+        steps{
+            echo "deploying the application..."
+            echo "deploying versio ${params.VERSION}"
+        }
     }
   }
 }
